7ce5decac3dacf899ce34cebfacc377305453833
terp1nator <bpls@outlook.com>
2018-08-02 16:25:59 -0400

    First checkin that also includes index.html and a .png file

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=irs-cs-demo-bot-Web-Deploy.pubxml -p:Password=R5q6ZHW1fg8rMdC1DlL0odxKrxneyvLpLcbgJy8pf7EAkw5ipYoCHv9PJn75
+
